91117c3f4e929c638374f24456756261ecc31cf1
dhanushsj-12 <test@test.com>
2026-07-10 00:42:51 +0530

    Completed Azure Blob Storage versioning and restore feature

--- app/config.py
+++ app/config.py
@@ -1,6 +1,4 @@
 import os
-from urllib.parse import quote_plus
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,18 +8,6 @@ class Config:
 
     SECRET_KEY = os.getenv("SECRET_KEY")
 
-    # Azure SQL Database Connection
-    username = os.getenv("SQL_USERNAME")
-    password = quote_plus(os.getenv("SQL_PASSWORD"))
-    server = os.getenv("SQL_SERVER")
-    database = os.getenv("SQL_DATABASE")
-
-    SQLALCHEMY_DATABASE_URI = (
-        f"mssql+pyodbc://{username}:{password}"
-        f"@{server}:1433/{database}"
-        "?driver=ODBC+Driver+18+for+SQL+Server"
-        "&Encrypt=yes"
-        "&TrustServerCertificate=no"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
